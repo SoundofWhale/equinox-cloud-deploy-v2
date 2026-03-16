aafabc4a399eb7f8da73a2093152a6c80c7c05b7
SoundofWhale <soundofwhale@example.com>
2026-03-16 11:19:43 +0100

    Fix svelte plugin compatibility and suppress a11y warnings

--- frontend/svelte.config.js
+++ frontend/svelte.config.js
@@ -6,6 +6,10 @@ const config = {
 	preprocess: vitePreprocess(),
 	kit: {
 		adapter: adapter()
+	},
+	onwarn: (warning, handler) => {
+		if (warning.code.includes('a11y-')) return;
+		handler(warning);
 	}
 };
 
